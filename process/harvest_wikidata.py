de2df8572fa5e4f2f7d80ecdf54e0348bbf02a8c
csae8092 <p.andorfer@gmail.com>
2025-04-07 21:15:47 +0200

    feat(data): script to scrape bith and death places added

---
+++ process/harvest_wikidata.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd
+from acdh_tei_pyutils.tei import TeiReader
+from acdh_tei_pyutils.utils import get_xmlid
+from acdh_xml_pyutils.xml import NSMAP
+from acdh_wikidata_pyutils import WikiDataPerson
+from tqdm import tqdm
+
+
+file = os.path.join("data", "indices", "listperson.xml")
+doc = TeiReader(file)
+failed = []
+entities = []
+items = doc.any_xpath(".//tei:person[./tei:idno[@subtype='wikidata']]")[:2]
+for x in tqdm(items, total=len(items)):
+    xml_id = get_xmlid(x)
+    wikidata = x.xpath("./tei:idno[@subtype='wikidata']", namespaces=NSMAP)[0].text
+    try:
+        wd_item = WikiDataPerson(wikidata)
+    except Exception as e:
+        failed.append[[xml_id, e]]
+        continue
+    ent_dict = wd_item.get_apis_entity()
+    ent_dict["place_of_birth"] = wd_item.place_of_birth
+    ent_dict["place_of_death"] = wd_item.place_of_death
+    entities.append(ent_dict)
+
+df = pd.DataFrame(entities)
+df.to_csv(os.path.join("process", "places.csv"), index=False)
+
+for x in failed:
+    print(x)
