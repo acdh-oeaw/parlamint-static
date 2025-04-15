3d4a540928aeafb3165c0543e8d5e04188638d14
kisram <Kinga.Sramo@oeaw.ac.at>
2025-04-15 14:33:22 +0200

    add people who were born/died at a place as notes to listplace.xml

--- process/create_listplace.py
+++ process/create_listplace.py
@@ -5,6 +5,12 @@ import lxml.etree as ET
 from acdh_tei_pyutils.tei import TeiReader
 from acdh_tei_pyutils.utils import get_xmlid
 from acdh_xml_pyutils.xml import NSMAP
+
+from collections import defaultdict
+
+# Initialize a dictionary to store people associated with each place
+place_to_people = defaultdict(lambda: {"birth": [], "death": []})
+
 # from acdh_wikidata_pyutils import WikiDataPlace
 # from tqdm import tqdm
 
@@ -21,12 +27,12 @@ from acdh_xml_pyutils.xml import NSMAP
 
 # df = pd.DataFrame(enriched_places)
 # df.to_csv(os.path.join("process", "enriched_places.csv"), index=False)
-file = os.path.join("data", "indices", "listplace.xml")
-doc = TeiReader(file)
-for bad in doc.any_xpath(".//tei:place[@xml:id]"):
+place_file = os.path.join("data", "indices", "listplace.xml")
+place_doc = TeiReader(place_file)
+for bad in place_doc.any_xpath(".//tei:place[@xml:id]"):
     bad.getparent().remove(bad)
 
-listperson = doc.any_xpath(".//tei:listPlace")[0]
+listplace = place_doc.any_xpath(".//tei:listPlace")[0]
 
 df = pd.read_csv(os.path.join("process", "enriched_places.csv"))
 
@@ -48,27 +54,34 @@ for i, row in sorted_df.iterrows():
     idno = ET.Element("{http://www.tei-c.org/ns/1.0}idno", attrib={"type": "URL", "subtype": "wikidata"})
     idno.text = f"http://www.wikidata.org/entity/{row['xml_id']}"
     place.append(idno)
-    listperson.append(place)
-
-doc.tree_to_file(file)
-
+    listplace.append(place)
+ 
 
 enriched_places = pd.read_csv(os.path.join("process", "enriched_places.csv"))
 persons = pd.read_csv(os.path.join("process", "places.csv"))
-file = os.path.join("data", "indices", "listperson.xml")
-doc = TeiReader(file)
+person_file = os.path.join("data", "indices", "listperson.xml")
+person_doc = TeiReader(person_file)
 
-for i, x in enumerate(doc.any_xpath(".//tei:person[@xml:id]")):
+for i, x in enumerate(person_doc.any_xpath(".//tei:person[@xml:id]")):
     xml_id = get_xmlid(x)
     try:
         current_person = persons[persons['xml_id'] == xml_id].iloc[0]
     except IndexError:
-        print(xml_id)
         continue
     try:
         place = x.xpath(".//tei:birth/tei:placeName", namespaces=NSMAP)[0]
         try:
-            place.attrib["key"] = current_person["place_of_birth"].split("/")[-1]
+            place_id = current_person["place_of_birth"].split("/")[-1]
+            place.attrib["key"] = place_id
+            # Locate the corresponding place in listplace.xml
+            listplace_place = place_doc.any_xpath(f".//tei:place[@xml:id='{place_id}']")[0]
+            note_grp = listplace_place.find("{http://www.tei-c.org/ns/1.0}noteGrp")
+            if note_grp is None:
+                note_grp = ET.Element("{http://www.tei-c.org/ns/1.0}noteGrp")
+                listplace_place.append(note_grp)
+            note = ET.Element("{http://www.tei-c.org/ns/1.0}note",attrib={"type": "births", "target": f"#{xml_id}"})
+            note.text = f"{current_person['name']}, {current_person['first_name']}"
+            note_grp.append(note)
         except AttributeError:
             pass
     except IndexError:
@@ -76,9 +89,21 @@ for i, x in enumerate(doc.any_xpath(".//tei:person[@xml:id]")):
     try:
         place = x.xpath(".//tei:death/tei:placeName", namespaces=NSMAP)[0]
         try:
-            place.attrib["key"] = current_person["place_of_death"].split("/")[-1]
+            place_id = current_person["place_of_death"].split("/")[-1]
+            place.attrib["key"] = place_id
+             # Locate the corresponding place in listplace.xml
+            listplace_place = place_doc.any_xpath(f".//tei:place[@xml:id='{place_id}']")[0]
+            note_grp = listplace_place.find("{http://www.tei-c.org/ns/1.0}noteGrp")
+            if note_grp is None:
+                note_grp = ET.Element("{http://www.tei-c.org/ns/1.0}noteGrp")
+                listplace_place.append(note_grp)
+            note = ET.Element("{http://www.tei-c.org/ns/1.0}note",attrib={"type": "deaths", "target": f"#{xml_id}"})
+            note.text = f"{current_person['name']}, {current_person['first_name']}"
+            note_grp.append(note)
         except AttributeError:
             pass
     except IndexError:
         pass
-doc.tree_to_file(file)
+
+place_doc.tree_to_file(place_file)
+person_doc.tree_to_file(person_file)
